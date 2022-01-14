eaa501e6670cc457e88815ef23ba57c5cb2d7024
aimperatori <aimperatori@ucs.br>
2022-01-14 14:51:49 -0300

    adicionado listView

--- app/src/main/java/com/example/homeassistantoff/MainActivity.kt
+++ app/src/main/java/com/example/homeassistantoff/MainActivity.kt
@@ -8,8 +8,12 @@ import android.widget.Toast
 import com.google.firebase.ktx.Firebase
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.ktx.messaging
+import android.content.Intent
+import com.example.homeassistantoff.List.ListActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,9 +40,13 @@ class MainActivity : AppCompatActivity() {
             // [END log_reg_token]
         }
 
-        Toast.makeText(this, "See README for setup instructions", Toast.LENGTH_SHORT).show()
 
+        val listBtn = findViewById<Button>(R.id.listBtn)
 
+        listBtn.setOnClickListener {
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
